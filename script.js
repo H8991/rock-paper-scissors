128f3a8a28402450701121edb75f7b158adfa5cf
harrison <166224036+H8991@users.noreply.github.com>
2025-07-03 13:50:42 +0100

    Added Pseudocode for project creation

---
+++ script.js
@@ -0,0 +1,17 @@
+// Write function that returns rock/paper/scissors for computer
+// Use Math.Random()
+
+/*Write function that returns rock/paper/scissors for Player
+Use prompt to get user input */
+
+// Keep track of the players score
+
+// keep track of the computers score
+
+// write play round function that compares choice to see who wins
+// Display the result (win/lose) 
+// increment scores accordingly
+
+// write play game - consists of 5 rounds - then displays overall winner
+
+
