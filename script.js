e7d8dda1833ea735a3a365d79516b028d281ba92
harrison <166224036+H8991@users.noreply.github.com>
2025-07-07 17:52:06 +0100

    added restart functionality

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-// #region ---------------Functions-------------------------
+//#region ---------------- Variables ------------------------
+let playerScore = 0;
+let computerScore = 0;
+let numOfGames = 0;
+let gameEnded = false;
+//#endregion
+
+//#region ---------- References to HTML elements ------------
+// Screen
+const screen = document.querySelector("#screen-container");
+//Buttons
+const rpsBtn = document.querySelectorAll(".rpsBtn");
+const rockBtn = document.querySelector(".rockBtn");
+const paperBtn = document.querySelector(".paperBtn");
+const scissorsBtn = document.querySelector(".scissorsBtn");
+// message text
+const message = document.querySelector("#message");
+// On-screen Scores
+const cScore = document.querySelector("#cScore");
+const pScore = document.querySelector("#pScore");
+// Reset
+const resetBtn = document.createElement("button");
+//#endregion
+
+// #region ---------------- Functions ------------------------
 function getComputerChoice() {
         let randomNumber = Math.random();
         let computerChoice;
@@ -15,81 +39,96 @@ function getComputerChoice() {
         return computerChoice;
 }
 
-function displayResults() {
-        //Win
-    if (playerScore > computerScore) {
-        alert(`Congratulations. You are the winner.\nYou won ${playerWins} out of ${numOfGames}.
-            Final Scores:
-            Your score: ${playerScore}
-            The Computer's score: ${computerScore}.
-            Who said AI could take over the world?`);
-    }
-        //lose
-    else if (playerScore < computerScore) {
-        alert(`Unlucky mate, the computer got ya. Better luck next time.
-            Final Scores:
-            Your score: ${playerScore}
-            The Computer's score: ${computerScore}.`);
-    }
-        //draw
-    else {
-        alert(`How boring. It was a draw.
-            Final Scores:
-            Your score: ${playerScore}
-            The Computer's score: ${computerScore}.`
-        );
-    }
-
-}
-
 function playRound(playerChoice) {
     let computerChoice = getComputerChoice();
 
+    if (gameEnded) {
+        return;
+    }
+    else {
+        
+        clearMessageClasses();
     if ((playerChoice == "paper" && computerChoice === "rock")
-        || (playerChoice == "scissors" && computerChoice === "paper")
-        || (playerChoice == "rock" && computerChoice === "scissors"))
+    || (playerChoice == "scissors" && computerChoice === "paper")
+    || (playerChoice == "rock" && computerChoice === "scissors"))
         {
         playerScore++;
+        message.classList.add("win");
         message.textContent = `You chose: ${playerChoice}.\nThe computer chose: ${computerChoice}.\nCongratulations, you won!`;
         }
     else if (playerChoice === computerChoice)
-    {
+        {
+        message.classList.add("draw");
         message.textContent =`You chose: ${playerChoice}.\nThe computer chose: ${computerChoice}. \nBoring...`;
-    }
+        }
     else
         {
         computerScore++
-        message.textContent = `You chose: ${playerChoice}.\nThe computer chose: ${computerChoice}. \nSorry, you lost this round.`;
+        message.classList.add("lose");
+        message.textContent = `You chose: ${playerChoice}.\nThe computer chose: ${computerChoice}.\nSorry, you lost this round.`;
         }
-        pScore.textContent = playerScore;
-        cScore.textContent = computerScore;
+    pScore.textContent = playerScore;
+    cScore.textContent = computerScore;
+    numOfGames++
+    checkGameOver(playerScore, computerScore);
+    }
+    
+}
+
+function checkGameOver(playerScore, computerScore) {
+
+    if ((playerScore == 5) || (computerScore==5)) {
+        gameEnded = true;
+        clearMessageClasses();
+        message.textContent = "Game Over.";
+    
+        // hide game buttons
+        rpsBtn.forEach(button => {
+            button.classList.add("hide");
+        });
+
+        // Add reset button
+        resetBtn.classList.remove("hide");
+        resetBtn.textContent = "New Game";
+        resetBtn.classList.add("rpsBtn");
+        screen.appendChild(resetBtn);
+
+        // On click of reset button:
+        resetBtn.addEventListener("click", () => {
+            message.textContent = "Rock, paper, or scissors?";
+
+            resetGame();
+        })
+    }
+}
+
+function clearMessageClasses() {
+    message.classList.remove("win");
+    message.classList.remove("lose");
+    message.classList.remove("draw");
+}
+
+function resetGame() {
+    // Hide reset button
+    resetBtn.classList.add("hide");
+
+    // re-enable game buttons
+    rpsBtn.forEach(button => {
+    button.classList.remove("hide");
+    });
+
+    playerScore = 0;
+    computerScore = 0;
+    pScore.textContent = playerScore;
+    cScore.textContent = computerScore;
+    numOfGames = 0;
+    gameEnded = false;
+
 }
 
 // #endregion
 
-//#region --------------- References to HTML elements --------------------
-//Buttons
-const rockBtn = document.querySelector(".rockBtn");
-const paperBtn = document.querySelector(".paperBtn");
-const scissorsBtn = document.querySelector(".scissorsBtn");
-
-// message text
-const message = document.querySelector("#message");
-
-// On-screen Scores
-const cScore = document.querySelector("#cScore");
-const pScore = document.querySelector("#pScore");
-//#endregion
-
-
-// #region ----------------Program-------------------------- 
-
-//#region ---------------- Variables ------------------------
-let playerScore = 0;
-let computerScore = 0;
-let numOfGames = 0;
-//#endregion
-
+// #region --------------- Event Listeners-------------------------- 
 // Event Listeners
 rockBtn.addEventListener("click", () => {
     playRound("rock");
@@ -100,12 +139,6 @@ paperBtn.addEventListener("click", () => {
 scissorsBtn.addEventListener("click", () => {
     playRound("scissors");
 })
-
-
-
-
-
-
 // #endregion
 
 
