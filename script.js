720692289c4b5dcb2e2c8fe6ded888f6a4af648c
harrison <166224036+H8991@users.noreply.github.com>
2025-07-07 18:54:06 +0100

    added start button function

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ let gameEnded = false;
 // Screen
 const screen = document.querySelector("#screen-container");
 //Buttons
+const startBtn = document.querySelector("#startBtn");
 const rpsBtn = document.querySelectorAll(".rpsBtn");
 const rockBtn = document.querySelector(".rockBtn");
 const paperBtn = document.querySelector(".paperBtn");
 const scissorsBtn = document.querySelector(".scissorsBtn");
 // message text
 const message = document.querySelector("#message");
+const scoreText = document.querySelectorAll(".score");
 // On-screen Scores
 const cScore = document.querySelector("#cScore");
 const pScore = document.querySelector("#pScore");
@@ -23,6 +25,33 @@ const resetBtn = document.createElement("button");
 //#endregion
 
 // #region ---------------- Functions ------------------------
+function welcomeUser() {
+    //Welcome Message
+    message.textContent = "Welcome to rock, paper, scissors.";
+    //Hide Elements
+    rpsBtn.forEach(button => {
+        button.classList.add("hide");
+    });
+    scoreText.forEach(element => {
+        element.classList.add("hide");
+    })
+    //Start Button
+    startBtn.addEventListener("click", () => {
+        //Show game buttons
+        rpsBtn.forEach(button => {
+        button.classList.remove("hide");
+        //Show scores
+        scoreText.forEach(element => {
+        element.classList.remove("hide");
+        //Remove start button
+        startBtn.remove();
+        //Set game message
+        message.textContent = "Rock, paper, or scissors?"
+    });
+    });
+    });
+}
+
 function getComputerChoice() {
         let randomNumber = Math.random();
         let computerChoice;
@@ -70,17 +99,23 @@ function playRound(playerChoice) {
     pScore.textContent = playerScore;
     cScore.textContent = computerScore;
     numOfGames++
-    checkGameOver(playerScore, computerScore);
+    gameOver(playerScore, computerScore);
     }
     
 }
 
-function checkGameOver(playerScore, computerScore) {
+function gameOver(playerScore, computerScore) {
 
     if ((playerScore == 5) || (computerScore==5)) {
         gameEnded = true;
         clearMessageClasses();
-        message.textContent = "Game Over.";
+        if (playerScore > computerScore){
+            message.textContent = "👑\nCongratulations.\nYou won!";
+        }
+        else {
+            message.textContent = "❌\nYou lost...";
+        }
+        
     
         // hide game buttons
         rpsBtn.forEach(button => {
@@ -130,6 +165,7 @@ function resetGame() {
 
 // #region --------------- Event Listeners-------------------------- 
 // Event Listeners
+
 rockBtn.addEventListener("click", () => {
     playRound("rock");
 })
@@ -140,6 +176,9 @@ scissorsBtn.addEventListener("click", () => {
     playRound("scissors");
 })
 // #endregion
+
+// -------------------- Program -------------------------------
+welcomeUser();
 
 
 
